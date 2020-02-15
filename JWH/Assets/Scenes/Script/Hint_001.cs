0ac103483ce23710a76b1bb1baa31fcdbfb5168f
Shikong <sc6733@nyu.edu>
2020-02-15 00:54:08 -0800

    THESIS W4
    20190215/UI WIP

---
+++ JWH/Assets/Scenes/Script/Hint_001.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hint_001 : MonoBehaviour
+{
+    public GameObject uiObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider player)
+    {
+        
+       if (player.gameObject.tag == "Player")
+        {   Debug.Log("colliding");
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+}
---
+++ JWH/Assets/Script/ClickDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDestroy : MonoBehaviour
+{
+
+    public GameObject Alarm;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+      
+        Alarm.SetActive(true);
+    }
+   
+
+}
+ 
