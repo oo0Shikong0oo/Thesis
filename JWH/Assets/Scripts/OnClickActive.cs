e836ebf03e1b94bf99cc84847263e4e7087aa30f
Shikong <sc6733@nyu.edu>
2020-02-20 09:48:08 -0800

    Adding insert scene
    Adding guess scene

---
+++ JWH/Assets/Scripts/OnClickActive.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickActive : MonoBehaviour
+{
+    public Rigidbody rb;
+     void OnMouseDown()
+    {
+        rb.useGravity = true;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
