9ff1200367d8825a3b7a402341576c7d55594744
Shikong <sc6733@nyu.edu>
2020-02-27 15:28:40 -0800

    Updating project files for w/c Feb 24

---
+++ JWH/Assets/Script/OpenPanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPanel : MonoBehaviour
+{
+    public GameObject Panel;
+    // Start is called before the first frame update
+
+        public void PanelOpener()
+    {
+        if(Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+    
+}
