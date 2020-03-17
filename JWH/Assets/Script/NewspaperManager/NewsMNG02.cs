6064b936586d812923015b74e1b0e4e13b868251
Shikong <sc6733@nyu.edu>
2020-03-17 13:44:05 -0700

    Transmission System Ready
    For port 1 & port 2

---
+++ JWH/Assets/Script/NewspaperManager/NewsMNG02.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NewsMNG02 : MonoBehaviour
+{
+    public Button[] taskButtons;
+    public static NewsMNG02 newspaper;
+    int numberActivated = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newspaper = this;
+    }
+
+    public void addHeadline(string HeadlineText)
+    {
+        if (numberActivated < taskButtons.Length)
+        {
+            taskButtons[numberActivated].GetComponentInChildren<Text>().text = HeadlineText;
+            numberActivated++;
+        }
+
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
