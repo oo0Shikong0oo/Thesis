c4c13a55161b79bbb75efc24b05899d58480f120
Shikong <sc6733@nyu.edu>
2020-03-17 18:32:31 -0700

    BUG Fix
    Fixing bugs from transmitter to view port

---
+++ JWH/Assets/Script/NewspaperManager/NSMNG3.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NSMNG3 : MonoBehaviour
+{
+    public Button[] taskButtons;
+    public static NSMNG3 newspaper;
+    int numberActivated = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newspaper = this;
+    }
+
+    public void addHeadline(string HeadlineText)
+    {
+        if (numberActivated < taskButtons.Length)
+        {
+            taskButtons[numberActivated].GetComponentInChildren<Text>().text = HeadlineText;
+            numberActivated++;
+        }
+        // Start is called before the first frame update
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ JWH/Assets/Script/NewspaperManager/TSKTR03.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TSKTR03 : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Debug.Log("taskTransmitterOn_003");
+        string texttosend = gameObject.GetComponentInChildren<TextMeshProUGUI>().text;
+        NewsMNG02.newspaper.addHeadline(texttosend);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
